be42ff4811f0b7e2904fb933b1f64460cb347fb4
Luca Eleto <lucavilarino321@gmail.com>
2025-02-24 16:53:36 -0300

    Create Agendamentos.py

---
+++ pages/Agendamentos.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import conexao
+import datetime
+
+
+st.set_page_config(page_title="Agendamentos", page_icon=":table:", layout="wide")
+st.header("Equilibrium Fisioterapia e Pilates")
+st.subheader("Agendamentos")
+
+data = pd.read_sql("SELECT id, paciente, dia_semana, hora_inicio, hora_fim, quantidade  FROM agenda", conexao.con)
+df = pd.DataFrame(data)
+
+df_editavel = st.data_editor(df)
+
+if st.button('Salvar'):
+    cursor_update = conexao.con.cursor()
+    update_agendamento = "UPDATE agenda SET paciente = %s, dia_semana = %s, quantidade = %s WHERE id = %s"
+    quantidade = int(df_editavel.iloc[0]['quantidade'])
+    id_agenda = int(df_editavel.iloc[0]['id'])
+    cursor_update.execute(update_agendamento, (df_editavel.iloc[0]['paciente'], df_editavel.iloc[0]['dia_semana'], quantidade, id_agenda))
+    conexao.con.commit()
+
+    st.success("Agendamentos salvos com sucesso!")
