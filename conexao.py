0fb228cc193d399a14c7d7a580a584afba647ca3
Luca Eleto <lucavilarino321@gmail.com>
2025-02-24 17:00:58 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -3,7 +3,7 @@ from mysql.connector import Error
 
 try:
     con = mysql.connector.connect(
-        host="192.1680.67",
+        host="192.168.0.67",
         user="root",
         password="clara02",
         database="fisio_equilibrium"
