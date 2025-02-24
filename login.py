749fd6162bab8b37ad3d7f4a167d138616254909
Luca Eleto <lucavilarino321@gmail.com>
2025-02-24 16:24:02 -0300

    Add files via upload

--- login.py
+++ login.py
@@ -3,27 +3,8 @@ import conexao
 
 st.set_page_config(page_title="Login", page_icon=":lock:", layout="wide")
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.write(' ')
-
-with col2:
-    st.image("logo-1.png", width=200)
-
-with col3:
-    st.write('')
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.write(' ')
-
-with col2:
-    st.header("Equilibrium Fisioterapia e Pilates")
-
-with col3:
-    st.write('')
-
+st.title("Equilibrium Fisioterapia e Pilates")
+st.subheader("Login")
 
 usuario = st.text_input('Usuário')
 senha = st.text_input('Senha', type="password")
